b3601c1b523f63726a3c7fca7cca23cba002bbcc
DAKS54 <sylvain.diallo@epitech.eu>
2026-04-25 16:26:18 +0000

    [Update] TaskInput, Item, List and page

---
+++ taskmanager/components/TaskList.js
@@ -0,0 +1,10 @@
+"use client";
+
+
+export default function TaskList() {
+  return (
+    <p className="mt-8 text-center text-gray-500 text-sm">
+      Aucune tâche pour le moment.
+    </p>
+  );
+}
